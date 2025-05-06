3f6b7656618228cd98320fb0b40bf2a13eeb3c12
B4rt0szKAG <bkaganiec@student.agh.edu.pl>
2025-05-06 12:04:09 +0200

    update 1

--- gui/dual_n_back.py
+++ gui/dual_n_back.py
@@ -1,48 +1,67 @@
 import tkinter as tk
 from tkinter import PhotoImage
-
-
-def show_square(position, image_label, image):
-    image_label.image = image
-    image_label.grid(row=position[0], column=position[1])
+from PIL import Image, ImageTk
+import os
 
 
 class GameScreen(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
+        self.frame_size = 600
+        self.cell_size = self.frame_size // 3
+        self.grid_labels =[[],[],[]]
 
-        self.columnconfigure((0,1,2,3,4), weight=1, uniform="1")
-        self.rowconfigure((0,1,2,3,4), weight=1, uniform="1")
+        # Konfiguracja wag: środkowe wiersze i kolumny mogą się skalować
+        for i in range(6):
+            self.columnconfigure(i, weight=1)
+            self.rowconfigure(i, weight=1)
 
+        # Label na górze w komórce (1,2)
         label1 = tk.Label(self, text="Witaj w Dual N-Back!", font=("Helvetica", 16))
-        label1.grid(row=0, column=2)
+        label1.grid(row=1, column=2, columnspan = 2, sticky="nesw", pady=10)
 
-        button1=tk.Button(self, text="voice", command= lambda: self.controller.show_frame("MenuScreen"))
-        button1.grid(row=4, column=1)
+        # Przyciski w (3,1) i (3,3)
+        button1 = tk.Button(self, text="voice", command=lambda: self.action())
+        button1.grid(row=5, column= 0 , columnspan = 3, sticky="nesw", pady=10)
 
-        button2 = tk.Button(self, text="position", command=lambda: self.controller.show_frame("MenuScreen"))
-        button2.grid(row=4, column=3)
+        button2 = tk.Button(self, text="position", command=lambda: self.action())
+        button2.grid(row=5, column=3, columnspan = 3, sticky="nesw", pady=10)
 
-        image = PhotoImage(file="resources/colored-squares/spr_square_blue.png")
-        image_label1 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label2 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label3 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label4 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label5 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label6 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label7 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label8 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label9 = tk.Label(self, image=image, bd=2, relief="solid")
-        image_label1.grid(row=1, column=1)
-        image_label2.grid(row=1, column=2)
-        image_label3.grid(row=1, column=3)
-        image_label4.grid(row=2, column=1)
-        image_label5.grid(row=2, column=2)
-        image_label6.grid(row=2, column=3)
-        image_label7.grid(row=3, column=1)
-        image_label8.grid(row=3, column=2)
-        image_label9.grid(row=3, column=3)
-        #show_square((2,1), image_label1, image)
+        # Centralny nierozszerzalny Frame (2,2)
+        center_frame = tk.Frame(self, width=self.frame_size, height=self.frame_size, bg="white", bd=2, relief="solid")
+        center_frame.grid(row=2, column=2, rowspan = 2, columnspan = 2)
+        center_frame.grid_propagate(False)
+        center_frame.pack_propagate(False)
+
+        #dodanie obrazu
+        img_path = os.path.join("resources", "colored-squares", "spr_square_blue.png")
+        base_img = Image.open(img_path)
+        scaled_img = base_img.resize((int( self.cell_size * 0.8), int( self.cell_size * 0.8)), Image.LANCZOS)
+        self.square_img = ImageTk.PhotoImage(scaled_img)
+
+
+
+        for i in range(3):
+            center_frame.columnconfigure(i, minsize= self.cell_size, weight=0)
+            center_frame.rowconfigure(i, minsize= self.cell_size, weight=0)
+            for j in range(3):
+                lbl = tk.Label(center_frame, bd=1, relief="solid", bg="white")
+                lbl.grid(row=i, column=j, padx=0, pady=0, sticky="nsew")
+                self.grid_labels[i].append(lbl)
+        center_frame.grid_propagate(False)
+
+
+
+
+        lbl_with_image = self.grid_labels[2][2]
+        lbl_with_image.configure(image=self.square_img)
+
+
+
+    def action(self):
+        print("action")
+
+
 
 
